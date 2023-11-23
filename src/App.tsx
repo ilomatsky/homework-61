82bc64947be05791d8462e097f93bb11239ed7d7
Ilya Lomatsky <ilomatsky@gmail.com>
2023-11-23 11:27:21 +0600

    Styling the app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ interface Country {
   alpha3Code: string;
   name: string;
   capital: string;
+  area: number;
+  flags: {
+    svg: string;
+  };
   population: number;
   borders: string[];
 }
@@ -14,6 +18,8 @@ function App() {
   const [countries, setCountries] = useState<Country[]>([]);
   const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
   const [countryInfo, setCountryInfo] = useState<Country | null>(null);
+  const [loadingCountries, setLoadingCountries] = useState(true);
+  const [loadingCountryInfo, setLoadingCountryInfo] = useState(false);
 
   useEffect(() => {
     const fetchCountries = async () => {
@@ -22,6 +28,8 @@ function App() {
         setCountries(response.data);
       } catch (error) {
         console.error('Error fetching countries:', error);
+      } finally {
+        setLoadingCountries(false);
       }
     };
 
@@ -31,12 +39,15 @@ function App() {
   useEffect(() => {
     const fetchCountryInfo = async () => {
       if (selectedCountry) {
+        setLoadingCountryInfo(true);
         try {
           const response = await axios.get(`https://restcountries.com/v2/alpha/${selectedCountry}`);
           setCountryInfo(response.data);
         } catch (error) {
           console.error('Error fetching country info:', error);
           setCountryInfo(null);
+        } finally {
+          setLoadingCountryInfo(false);
         }
       }
     };
@@ -47,31 +58,57 @@ function App() {
   return (
     <div className="App">
       <div className="country-list">
-        <ul>
-          {countries.map((country) => (
-            <li className="country" key={country.alpha3Code} onClick={() => setSelectedCountry(country.alpha3Code)}>
-              {country.name}
-            </li>
-          ))}
-        </ul>
+        {loadingCountries ? (
+          <p>Loading countries...</p>
+        ) : (
+          <ul>
+            {countries.map((country) => (
+              <li
+                key={country.alpha3Code}
+                className="country"
+                onClick={() => {
+                  setSelectedCountry(country.alpha3Code);
+                  setCountryInfo(null); // Сброс информации о стране при клике для показа прелоадера
+                }}
+              >
+                {country.name}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
 
       <div className="country-info">
         {selectedCountry ? (
-          <div>
-            <h1>{countryInfo?.name}</h1>
-            {countryInfo ? (
-              <div>
-                <p>Capital: {countryInfo.capital}</p>
-                <p>Population: {countryInfo.population}</p>
-                <p>Borders with: {countryInfo?.borders ? countryInfo.borders.join(', ') : 'the country has no land borders'}</p>
-              </div>
+          <div className="country-data">
+            {loadingCountryInfo ? (
+              <p>Loading country information...</p>
             ) : (
-              <p>Error when getting information about the country</p>
+              <div>
+                <h1>{countryInfo?.name}</h1>
+                {countryInfo ? (
+                  <div>
+                    <img className="flag" src={countryInfo.flags.svg} alt="flag"/>
+                    <p>Capital: {countryInfo.capital}</p>
+                    <p>Population: {countryInfo.population}</p>
+                    <p>Area: {countryInfo.area} km²</p>
+                    <p>
+                      Borders with:{' '}
+                      {countryInfo?.borders
+                        ? countryInfo.borders.length > 0
+                          ? countryInfo.borders.join(', ')
+                          : 'the country has no land borders'
+                        : 'loading...'}
+                    </p>
+                  </div>
+                ) : (
+                  <p>Error when getting information about the country</p>
+                )}
+              </div>
             )}
           </div>
         ) : (
-          <p>Select a country</p>
+          <p className="condition">Select a country</p>
         )}
       </div>
     </div>
